e54576d63e3508cccef4e7d5ee35c06f09ece215
Akagiwyf <11912913@mail.sustech.edu.cn>
2022-10-04 17:22:22 +0800

    create numpy array for ETT dataset.

--- util/dataset.py
+++ util/dataset.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from param import ECL_PATH, WTH_PATH, KDD_PATH, ETT_PATH_DIR
 import locale
+import numpy as np
+import os
 
 
 class ECL:
@@ -10,10 +12,19 @@ class ECL:
 
 class ETT:
     def __init__(self):
-        self.df_h1 = pd.read_csv(ETT_PATH_DIR + "/ETTh1.csv")
-        self.df_h2 = pd.read_csv(ETT_PATH_DIR + "/ETTh2.csv")
-        self.df_m1 = pd.read_csv(ETT_PATH_DIR + "/ETTm1.csv")
-        self.de_m2 = pd.read_csv(ETT_PATH_DIR + "/ETTm2.csv")
+        self.df_h1 = np.array(pd.read_csv(ETT_PATH_DIR + "/ETTh1.csv"))
+        self.df_h2 = np.array(pd.read_csv(ETT_PATH_DIR + "/ETTh2.csv"))
+        self.df_m1 = np.array(pd.read_csv(ETT_PATH_DIR + "/ETTm1.csv"))
+        self.de_m2 = np.array(pd.read_csv(ETT_PATH_DIR + "/ETTm2.csv"))
+        
+
+    def processed_data(self, arr):
+        x_arr = []
+        y_arr = []
+        for i in range(len(arr)):
+            x_arr.append(arr[i][0])
+            y_arr.append(arr[i][1:])
+        return np.array(x_arr), np.array(y_arr)
 
 
 class WTH:
@@ -27,4 +38,6 @@ class WTDATA:
 
 
 if __name__ == '__main__':
-    E = ECL()
+    E = ETT()
+    print(E.processed_data(E.df_h1)[0])
+    print(E.processed_data(E.df_h1)[1])
