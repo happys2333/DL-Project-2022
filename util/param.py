067117264d8856af6aaf6edd26d68f7637e773c2
zrh-LegionWin <11912614@mail.sustech.edu.cn>
2022-10-05 20:53:01 +0800

    update param: add path_parse() to make path independent on files

--- util/param.py
+++ util/param.py
@@ -1,10 +1,20 @@
+import os
+
+self_path = os.path.split(__file__)[0]
+
+
+def path_parse(target_path):
+    return os.path.join(self_path, target_path)
+
+
 # data set path
 
-ETT_PATH_DIR = "../dataset/ETT-small"
-ECL_PATH = "../dataset/ECL.csv"
-WTH_PATH = "../dataset/WTH.csv"
-KDD_PATH = "../dataset/wtbdata_245days.csv"
+ETT_PATH_DIR = path_parse("../dataset/ETT-small")
+ECL_PATH = path_parse("../dataset/ECL.csv")
+WTH_PATH = path_parse("../dataset/WTH.csv")
+KDD_PATH = path_parse("../dataset/wtbdata_245days.csv")
 
 # result path
 
-ECL_PRED_PATH = "../submodule/informer2020/results/informer_ECL_ftM_sl96_ll48_pl24_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_0"
+ECL_PRED_PATH = path_parse(
+    "../submodule/informer2020/results/informer_ECL_ftM_sl96_ll48_pl24_dm512_nh8_el2_dl1_df2048_atprob_fc5_ebtimeF_dtTrue_mxTrue_test_0")
