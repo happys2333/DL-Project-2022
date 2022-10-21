8061c30a83ac8f7107a982f483cf1b9a5b67655e
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-10-21 13:36:28 +0800

    add function of drawing comparison bar and add some pngs

--- util/drawtools.py
+++ util/drawtools.py
@@ -63,41 +63,86 @@ def plot_pred(pred_len=24, dataset="ETTh1", model="informer", features="S", show
     return ground_truth, target_pred
 
 
-def draw_result_bar():
-    result_path = "../result/multivariate/sq96_lb48_pre192_mul_wth.csv"
-    result = pd.read_csv(result_path)
-
-    size = 2
-
-    x = np.arange(size)
-
-    # 有a/b两种类型的数据，n设置为2
-    total_width, n = 0.6, 3
-    # 每种类型的柱状图宽度
-    width = total_width / n
-
-    data = [[0.542752, 0.525745],
-            [0.587715, 0.559554],
-            [0.745868, 0.622514]]
+def draw_result_bars():
     colors = ["#4E79A7", "#A0CBE8", "#F28E2B", "#FFBE7D", "#59A14F", "#8CD17D", "#B6992D", "#F1CE63", "#499894",
               "#86BCB6", "#E15759", "#E19D9A"]
-    # 重新设置x轴的坐标
-    x = x - (total_width - width) / n
-    # 画柱状图
-    plt.bar(x, data[0], width=width, label="Informer", color=colors[0])
-    plt.bar(x + width, data[1], width=width, label="Autoformer", color=colors[1])
-    plt.bar(x + 2 * width, data[2], width=width, label="LSTM", color=colors[2])
-    plt.xticks(np.arange(size), ("MSE", "MAE"))
-    # 显示图例
-    # plt.figure(dpi=300,figsize=(24,24))
-    plt.legend(loc='lower right', prop={"family": "Times New Roman"})
-    plt.xlabel("Metric Comparison", fontname="Times New Roman")
-    plt.ylabel("Value of MSE or MAE", fontname="Times New Roman")
-    # 显示柱状图
-    plt.show()
+    total_width = 0.6
+
+    def draw_subplot(model_names: list, dataset_names: list, origin_data, start_pos: int, metric_name, pre_len=192,
+                     feature_type="M"):
+        plot_num = len(origin_data)
+        pos = start_pos
+        end_pos = pos + plot_num - 1
+        for p_n in range(plot_num):
+            model_name = model_names[p_n]
+            n = len(model_name)  # n is the number of model
+            width = total_width / n  # 每种类型的柱状图宽度
+            f_type_name = "multivariate" if feature_type == "M" else "univariate"
+
+            # 重新设置x轴的坐标
+            # range_x is the number of datasets
+            dataset_name = dataset_names[p_n]
+            range_x = len(dataset_name)
+            x = np.arange(range_x)
+            x = x - (total_width - width) / 2
+
+            # 画柱状图
+            plt.subplot(pos)
+            for i in range(n):
+                plt.bar(x + i * width, origin_data[p_n][i], width=width, label=model_name[i], color=colors[i])
+            plt.xticks(np.arange(range_x), dataset_name)
+            # 显示图例
+            if pos == start_pos:
+                plt.title("Models comparison with %s prediction of %d length" % (f_type_name, pre_len))
+            plt.legend()
+            if pos == end_pos:
+                plt.xlabel("Datasets", fontname="Times New Roman")
+            plt.ylabel("%s" % metric_name[p_n], fontname="Times New Roman")
+            pos += 1
+
+        plt.show()
+
+    dataset = [["ECL", "ETTh1", "WTH"], ["ECL", "ETTh1", "WTH"]]
+
+    # M
+    f_t1 = "M"
+    models1 = [["Autoformer", "Informer", "LSTM"], ["Autoformer", "Informer", "LSTM"]]
+    pred_len1 = 192
+    metric1 = ["MSE", "MAE"]
+    pos1 = 211
+    # data[i] means ith model's metric on len(data[i]) datasets
+    data1 = [[[0.193802, 0.535738, 0.587715],
+              [0.284998, 1.130280, 0.542752],
+              [0.394332, 1.266324, 0.745868]],
+             [[0.306151, 0.487452, 0.559554],
+              [0.374030, 0.875960, 0.525745],
+              [0.451243, 0.840875, 0.622514]]
+             ]
+    draw_subplot(model_names=models1, dataset_names=dataset, origin_data=data1, start_pos=pos1, metric_name=metric1,
+                 feature_type=f_t1, pre_len=pred_len1)
+
+    # S
+    f_t2 = "S"
+    models2 = [["Autoformer", "Informer", "LSTM"],
+               ["Autoformer", "Informer", "LSTM"]]
+    pred_len2 = 168
+    metric2 = ["MSE", "MAE"]
+    pos2 = 211
+    # data[i] means ith model's metric on len(data[i]) datasets
+    data2 = [[[0.384328, 0.142934, 0.243901],
+              [0.336435, 0.235669, 0.250150],
+              # [0.394037, 0.430356, 0.202001],
+              [0.319112, 0.283552, 0.334202]],
+             [[0.466045, 0.292151, 0.367090],
+              [0.409530, 0.419669, 0.376537],
+              # [0.462601, 0.585273, 0.336203],
+              [0.409603, 0.424886, 0.435629]]
+             ]
+    draw_subplot(model_names=models2, dataset_names=dataset, origin_data=data2, start_pos=pos2, metric_name=metric2,
+                 feature_type=f_t2, pre_len=pred_len2)
 
 
 if __name__ == "__main__":
     # plot_pred(168, "ETTh1", "informer", features="S")
-    draw_result_bar()
+    draw_result_bars()
     # plot_comparison(datasets=["ECL"], models=["informer", "autoformer","reformer"], pred_len=168)
