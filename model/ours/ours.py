e49bff8ebcaee27c498f718eaacdcfe6d585eb6d
happys <happys2333@outlook.com>
2022-11-14 23:20:15 +0800

    separate module

---
+++ model/ours/ours.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class Ourformer(nn.Module):
+    def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len,
+                 factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512,
+                 dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
+                 output_attention=False, distil=True, mix=True,
+                 device=torch.device('cuda:0')):
+        super(Ourformer, self).__init__()
+
+    def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec,
+                enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
+        pass
