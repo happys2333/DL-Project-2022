6615061a86bd1e150d1d9ac079bc0c2febddddbc
zrh-LegionWin <11912614@mail.sustech.edu.cn>
2022-11-29 22:23:37 +0800

    add informer training script

---
+++ train/script/train_informer2020.sh
@@ -0,0 +1,2 @@
+# S
+python -u main_informer.py --model informer --data ECL --root_path ../../dataset/ --features S --freq h --seq_len 168 --label_len 168 --pred_len 336 --e_layers 2 --d_layers 1 --attn prob --des 'Exp' --itr 1 --batch_size 4
