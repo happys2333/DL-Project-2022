637a20161a8a5068e2b35849ae1904f32995186b
zrh-LegionLinux <11912614@mail.sustech.edu.cn>
2022-12-01 19:59:06 +0800

    add more metrics and training scripts

--- train/script/train_autoformer.sh
+++ train/script/train_autoformer.sh
@@ -1,2 +1,4 @@
 # M
-python -u run.py   --is_training 1   --root_path ../../dataset/ETT-small/ --data_path ETTh1.csv  --model_id ETTh1_96_96  --model Autoformer  --data ETTh1 --features M --seq_len 96 --label_len 48 --pred_len 96 --e_layers 2 --d_layers 1 --factor 3 --enc_in 7 --dec_in 7 --c_out 7 --des 'Exp' --itr 1 --batch_size 16
+python -u run.py   --is_training 1   --root_path ../../dataset/ETT-small/ --data_path ETTh1.csv  --model_id ETTh1_96_96  --model Autoformer  --data ETTh1 --features M --seq_len 96 --label_len 48 --pred_len 96 --e_layers 2 --d_layers 1 --factor 3 --enc_in 7 --dec_in 7 --c_out 7 --des 'Exp' --itr 1 --batch_size 32
+
+python -u run.py   --is_training 1   --root_path ../../dataset/ --data_path ECL.csv  --model_id ECL_96_96  --model Autoformer  --data custom --features M --seq_len 96 --label_len 48 --pred_len 96 --e_layers 2 --d_layers 1 --factor 3 --enc_in 321 --dec_in 321 --c_out 321 --des 'Exp' --itr 2 --batch_size 16 --target MT_320
